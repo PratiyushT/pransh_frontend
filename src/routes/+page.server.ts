0c64634c974986ad93cfb74e64eee8898cbbd5fa
Pratiyush <aloomataloo@gmail.com>
2025-04-30 21:38:51 -0500

    Fixed some issues with address in account page.

--- src/routes/+page.server.ts
+++
@@ -1,9 +0,0 @@
- // src/routes/+page.server.ts
-import type { PageServerLoad } from './$types'
-import { fetchHomepageData } from './+server'
-
-
-export const load: PageServerLoad = async () => {
-  // runs only on the server
-  return fetchHomepageData()
-}
